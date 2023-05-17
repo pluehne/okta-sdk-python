2c4a998cfebbb0e8f158a724f4c8721497a1e05d
Patrick Lühne <patrick@luehne.de>
2023-05-17 22:40:33 +0200

    Fix unchecked accesses in error type constructor

--- okta/errors/okta_api_error.py
+++ okta/errors/okta_api_error.py
@@ -4,14 +4,14 @@ from okta.errors.error import Error
 class OktaAPIError(Error):
     def __init__(self, url, response_details, response_body):
         self.status = response_details.status
-        self.error_code = response_body["errorCode"]
+        self.error_code = response_body.get("errorCode", None)
         self.error_summary = response_body.get("errorSummary", "")
         self.error_causes = response_body.get("errorCauses", [])
         error_causes_string = ". ".join(list(map(lambda x:
                                                  x.get("errorSummary", ""),
                                                  self.error_causes)))
-        self.error_link = response_body["errorLink"]
-        self.error_id = response_body["errorId"]
+        self.error_link = response_body.get("errorLink", None)
+        self.error_id = response_body.get("errorId", None)
 
         self.url = url
         self.headers = response_details.headers
